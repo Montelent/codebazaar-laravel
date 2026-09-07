11de214a6c34b6a4bfe30c1937afa0dbb1088025
Montel Anthony <58714976+Montelent@users.noreply.github.com>
2026-09-07 16:15:08 +0200

    Rename Navigation nav item to Menus

--- resources/views/layouts/partials/admin-nav.blade.php
+++ resources/views/layouts/partials/admin-nav.blade.php
@@ -18,7 +18,7 @@
 <a href="{{ route('admin.settings.hub') }}" class="block rounded-lg px-3 py-2 hover:bg-slate-800">Settings hub</a>
 <a href="{{ route('admin.settings.payments') }}" class="block rounded-lg px-3 py-2 hover:bg-slate-800">Payments</a>
 <a href="{{ route('admin.settings.storage') }}" class="block rounded-lg px-3 py-2 hover:bg-slate-800">File storage</a>
-<a href="{{ route('admin.settings.navigation') }}" class="block rounded-lg px-3 py-2 hover:bg-slate-800">Navigation</a>
+<a href="{{ route('admin.settings.navigation') }}" class="block rounded-lg px-3 py-2 hover:bg-slate-800">Menus</a>
 <a href="{{ route('admin.settings.header_footer') }}" class="block rounded-lg px-3 py-2 hover:bg-slate-800">Header / footer</a>
 <a href="{{ route('admin.settings.ads') }}" class="block rounded-lg px-3 py-2 hover:bg-slate-800">Ad placements</a>
 <a href="{{ route('admin.settings.schema') }}" class="block rounded-lg px-3 py-2 hover:bg-slate-800">Schema SEO</a>
