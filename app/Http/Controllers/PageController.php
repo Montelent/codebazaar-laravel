a910b9478a7f3d39eb61cada1e3fbc330d9287b8
Montel Anthony <58714976+Montelent@users.noreply.github.com>
2026-09-08 01:35:23 +0200

    Pass SEO payload to CMS page views

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\CmsPage;
+use App\Support\Seo;
 
 class PageController extends Controller
 {
     public function show(string $slug)
     {
         $page = CmsPage::where('slug', $slug)->where('status', 'published')->firstOrFail();
-        return view('pages.show', compact('page'));
+        $seo = Seo::make($page->seoPayload());
+
+        return view('pages.show', compact('page', 'seo'));
     }
 }
