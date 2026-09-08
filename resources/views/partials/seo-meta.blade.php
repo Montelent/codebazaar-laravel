7f3a7124ed67112dbe1fb750ccdb3db06e615c5a
Montel Anthony <58714976+Montelent@users.noreply.github.com>
2026-09-08 02:02:06 +0200

    Complete OG/Twitter/robots meta tags with absolute URLs

--- resources/views/partials/seo-meta.blade.php
+++ resources/views/partials/seo-meta.blade.php
@@ -1,6 +1,6 @@
 @php
   /** @var array $seo */
-  $seo = $seo ?? \App\Support\Seo::make();
+  $seo = \App\Support\Seo::make(is_array($seo ?? null) ? $seo : []);
 @endphp
 <title>{{ $seo['title'] }}</title>
 <meta name="description" content="{{ $seo['description'] }}">
@@ -8,17 +8,25 @@
 <meta name="keywords" content="{{ $seo['keywords'] }}">
 @endif
 <meta name="robots" content="{{ $seo['robots'] }}">
+<meta name="googlebot" content="{{ $seo['robots'] }}">
 <link rel="canonical" href="{{ $seo['canonical'] }}">
 
+{{-- Open Graph --}}
 <meta property="og:type" content="{{ $seo['og_type'] }}">
 <meta property="og:site_name" content="{{ $seo['og_site_name'] }}">
 <meta property="og:title" content="{{ $seo['og_title'] }}">
 <meta property="og:description" content="{{ $seo['og_description'] }}">
 <meta property="og:url" content="{{ $seo['og_url'] }}">
 <meta property="og:image" content="{{ $seo['og_image'] }}">
+<meta property="og:image:alt" content="{{ $seo['og_title'] }}">
 <meta property="og:locale" content="{{ str_replace('_', '-', app()->getLocale()) }}">
 
+{{-- Twitter --}}
 <meta name="twitter:card" content="{{ $seo['twitter_card'] }}">
 <meta name="twitter:title" content="{{ $seo['twitter_title'] }}">
 <meta name="twitter:description" content="{{ $seo['twitter_description'] }}">
 <meta name="twitter:image" content="{{ $seo['twitter_image'] }}">
+
+{{-- Helpful defaults --}}
+<meta name="format-detection" content="telephone=no">
+<meta name="theme-color" content="{{ $seo['theme_color'] ?? '#82b440' }}">
