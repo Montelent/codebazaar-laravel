1963257329b0eeebdfb9dad984f346d23323c680
Montel Anthony <58714976+Montelent@users.noreply.github.com>
2026-09-14 00:31:36 +0200

    Extend User model with credits and referral relations

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
@@ -13,6 +14,7 @@ class User extends Authenticatable
     protected $fillable = [
         'username', 'name', 'email', 'password', 'role',
         'avatar_url', 'bio', 'newsletter',
+        'credit_balance', 'referral_code', 'referred_by',
     ];
 
     protected $hidden = ['password', 'remember_token'];
@@ -23,7 +25,17 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
             'newsletter' => 'boolean',
+            'credit_balance' => 'decimal:2',
         ];
+    }
+
+    protected static function booted(): void
+    {
+        static::creating(function (User $user) {
+            if (empty($user->referral_code)) {
+                $user->referral_code = strtoupper(Str::random(8));
+            }
+        });
     }
 
     public function isAdmin(): bool
@@ -44,5 +56,30 @@ class User extends Authenticatable
     public function wishlist()
     {
         return $this->belongsToMany(Item::class, 'wishlist_items')->withTimestamps();
+    }
+
+    public function creditTransactions()
+    {
+        return $this->hasMany(CreditTransaction::class)->orderByDesc('id');
+    }
+
+    public function supportTickets()
+    {
+        return $this->hasMany(SupportTicket::class);
+    }
+
+    public function referrer()
+    {
+        return $this->belongsTo(User::class, 'referred_by');
+    }
+
+    public function referrals()
+    {
+        return $this->hasMany(User::class, 'referred_by');
+    }
+
+    public function hasCredits(float $amount): bool
+    {
+        return (float) $this->credit_balance >= $amount;
     }
 }
